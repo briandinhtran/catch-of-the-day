836e8ffc2da3f5159430e6b4af8bc13f5ff59d10
Brian Tran <briandinhtran@outlook.com>
2017-08-14 14:14:11 -0700

    persisting our state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCNJNja2qYiO7WC2tpqP4ddq1PzZMbMhMU",
+  authDomain: "catch-of-the-day-brian-tran.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-brian-tran.firebaseio.com",
+});
+
+export default base;
